936e2b241fafb817cdd51955527b3c842dd3a40e
Cristian <mao.salgado7@gmail.com>
2025-11-24 18:44:36 -0500

    Implementacion del dashboard

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "@/pages/auth/Login";
 import Register from "@/pages/auth/Register";
 import NotFound from "@/pages/NotFound";
+import Dashboard from "@/pages/dashboard/dashboard";
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,8 @@ export default function AppRouter() {
             { /* AUTH */ }
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
+
+            <Route path="/home" element={<Dashboard />} />
 
             { /* 404 */ }
             <Route path="*" element={<NotFound />} />
